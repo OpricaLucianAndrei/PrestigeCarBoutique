88b076ffb7431d6178ee651e9534b4b50a9b50b9
OpricaLucianAndrei <opricalucian4@gmail.com>
2024-06-08 16:53:02 +0200

    initial commit

---
+++ src/test/java/it/capstone/prestigecarboutique/PrestigeCarBoutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package it.capstone.prestigecarboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestigeCarBoutiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
